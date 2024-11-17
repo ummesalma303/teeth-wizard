321ad5cdf2bb9e4fc0ae6a9f440c52e52e73922f
ummesalma303 <ummesalma333999@gmail.com>
2024-11-17 07:32:48 +0600

    complete basic setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            i am nav
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/firebase/Firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlv_7KujT0DPpatXgT9yhCc3AO8zHiZq8",
+  authDomain: "teeth-wizard-b77e6.firebaseapp.com",
+  projectId: "teeth-wizard-b77e6",
+  storageBucket: "teeth-wizard-b77e6.firebasestorage.app",
+  messagingSenderId: "520954931417",
+  appId: "1:520954931417:web:4c43ec3970201e3f06f6c8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
